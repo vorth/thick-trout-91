49b38f8231ad256d028095f01c89cbf49dddb96d
Scott Vorthmann <scott@vorthmann.org>
2023-10-26 17:10:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-import {
-  OAuthApp,
-  createAWSLambdaAPIGatewayV2Handler,
-} from "@octokit/oauth-app";
+
+import { OAuthApp, createWebWorkerHandler } from "@octokit/oauth-app";
 const app = new OAuthApp({
   clientType: "oauth-app",
-  clientId: Deno.env.get( 'GITHUB_CLIENT_ID' ),
-  clientSecret: Deno.env.get( 'GITHUB_CLIENT_SECRET' ),
+  clientId: Deno.env .get( 'GITHUB_CLIENT_ID' ),
+  clientSecret: Deno.env .get( 'GITHUB_CLIENT_SECRET' ),
 });
 
-export const handler = createAWSLambdaAPIGatewayV2Handler(app, {
+const handleRequest = createWebWorkerHandler(app, {
   pathPrefix: "/api/github/oauth",
 });
 
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 // can now receive user authorization callbacks at /api/github/oauth/callback
